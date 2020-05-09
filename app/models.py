c33f425cfbe4eb2f08518fec0495d7052ee5cca0
RioAdiNugraha <rioadinugraha@yahoo.com>
2020-05-09 23:18:19 +0700

    dependency fixes

--- app/models.py
+++ app/models.py
@@ -1,13 +1,16 @@
+from app import login
 from app import db
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
-
-class User(db.Model):
+from hashlib import md5
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer,primary_key=True)
     username = db.Column(db.String(128),index=True,unique=True,nullable=False)
+    email = db.Column(db.String(128), index=True, unique=True, nullable=False)
     password = db.Column(db.String(128),nullable=False)
     user_level = db.Column(db.Integer,nullable= False)
+
 
     def __repr__(self):
         return '<User{}>'.format(self.username)
@@ -18,9 +21,19 @@ class User(db.Model):
     def check_password(self,password):
         return check_password_hash(self.password,password)
 
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
 
---
+++ migrations/versions/82dd8d3a4469_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 82dd8d3a4469
+Revises: bfb18b9b357d
+Create Date: 2020-05-09 23:01:16.273487
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '82dd8d3a4469'
+down_revision = 'bfb18b9b357d'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
