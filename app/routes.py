330a94eef2bfe4874d3d9afd9058e498c9b8a81b
Robert <robertreden2408@gmail.com>
2020-05-12 00:11:43 +0700

    ui testing

--- app/routes.py
+++ app/routes.py
@@ -50,7 +50,7 @@ def login():
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('index')
         return redirect(next_page)
-    return render_template('login.html',title='sign In', form = form)
+    return render_template('login.html',title='Sign In', form = form)
 
 
 @app.route('/logout')
